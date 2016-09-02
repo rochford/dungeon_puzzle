c415d0c7fca777dad7a4c45f9a2562c4af0546d2
Timothy <timothy.rochford@gmail.com>
2016-09-02 13:31:15 +0300

    First implementation.

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
 # dungeon_puzzle
 Coding test (time constrained solution) - HitPoints required to traverse a dungeon grid. 
+
+
+Notes: This implementation would done in about 3 hours.
+Some of that time was spent on utility functions like printing the dungeon and generating a random dungeon.
+
+## Rules (as i remember them)
+1. Knight starts in Top left corner.
+2. Knight needs to travel to Princess in bottom right corner of dungeon.
+3. Knight can only move RIGHT or DOWN.
+4. Each square has a HitPoint value to be added or deducted from the Knight.
+
+## Task
+Calculate the minimum amount of HitPoints the Knight initially needs to reach the Princess.
+
+## Compiling
+g++ -std=c++0x dungeon.cpp
+
+## Drawbacks
+
+This implementation does not process all of the possible routes through the dungeon. Instead the implemention
+randomly decides to go DOWN or RIGHT. If the completed route is better than any of the previous random routes
+travelled then the required Knight HitPoints is updated. Several iterations of the possible routes are examined,
+the amount of iterations depends on the size of the dungeon.
+
+For large dungeon grids the amount of iterations may be too much. For dungeon grids upto about 8x8 it seems okay.
+
+## Sample Output
+
+$ ./a.out 1 1
+ 2,
+Minimum Knight HitPoints to rescue princess: -2
+$ ./a.out 2 2
+ 0, -2,
+-4, -3,
+Minimum Knight HitPoints to rescue princess: 5
+$ ./a.out 2 2
+ 1, -3,
+-1, -2,
+Minimum Knight HitPoints to rescue princess: 2
+$ ./a.out 2 2
+-4, -3,
+ 1, -3,
+Minimum Knight HitPoints to rescue princess: 6
+$ ./a.out 2 2
+-3,  0,
+-3,  1,
+Minimum Knight HitPoints to rescue princess: 2
+$ ./a.out 2 2
+ 1, -3,
+-3, -3,
+Minimum Knight HitPoints to rescue princess: 5
+$ ./a.out 3 3
+-3,  0,  0,
+-1,  0,  0,
+ 2, -1, -4,
+Minimum Knight HitPoints to rescue princess: 7
+$ ./a.out 3 3
+ 0, -2,  2,
+ 1, -2, -1,
+ 0, -1, -1,
+Minimum Knight HitPoints to rescue princess: 1
+$ ./a.out 3 3
+ 1, -3,  1,
+ 0,  0, -3,
+ 2,  1, -3,
+Minimum Knight HitPoints to rescue princess: -1
+$ ./a.out 3 3
+ 1, -3, -2,
+-2, -2, -1,
+ 2, -1,  0,
+Minimum Knight HitPoints to rescue princess: 0
+$ ./a.out 3 3
+-1, -3,  2,
+ 0, -4,  0,
+ 1, -2, -1,
+Minimum Knight HitPoints to rescue princess: 3
+$ ./a.out 4 4
+ 2,  1, -3, -3,
+-2,  0, -2, -2,
+-4,  1,  0, -3,
+ 0, -2, -2, -3,
+Minimum Knight HitPoints to rescue princess: 1
+$ ./a.out 4 4
+-3, -2,  0,  2,
+ 2, -1,  2, -3,
+ 2, -1,  2, -3,
+-4,  0,  2,  2,
+Minimum Knight HitPoints to rescue princess: -6
+$ ./a.out 3 2
+-1, -4,
+ 1, -3,
+-1,  1,
+Minimum Knight HitPoints to rescue princess: 0
+$ ./a.out 3 2
+-4, -2,
+-2,  1,
+ 0, -1,
+Minimum Knight HitPoints to rescue princess: 6
+$ ./a.out 3 5
+-1, -1,  0,  2,  0,
+ 2,  0, -2, -2, -2,
+ 0,  1, -4,  0, -1,
+Minimum Knight HitPoints to rescue princess: 3
+$ ./a.out 3 5
+-1, -4,  0, -3,  2,
+-4,  2,  1,  2,  1,
+ 2,  2,  0, -2,  1,
+Minimum Knight HitPoints to rescue princess: -2
+$ ./a.out 3 5
+-2, -1, -2,  0, -2,
+ 1, -4, -2, -1,  0,
+-2,  1, -2, -3,  2,
+Minimum Knight HitPoints to rescue princess: 4
+
---
+++ dungeon.cpp
@@ -0,0 +1,135 @@
+//
+// Tim Rochford, 2016
+//
+#include <vector>
+#include <cmath>  // abs()
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <limits> // std::numeric_limits
+#include <string> // stoi
+
+using namespace std;
+
+void printDungeon(const vector<vector<int>>& d) {
+    for (auto row : d) {
+        for (auto cell : row) {
+            // line up the cells...
+            if (cell < 0)
+                cout << cell << ", ";
+            else
+                cout << " " << cell << ", ";
+        }
+        cout << endl;
+    }
+}
+
+vector<vector<int>> randomMatrix(int rows, int columns, int min, int max) {
+
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine generator (seed);
+    uniform_int_distribution<int> distribution(min,max);
+
+    vector<vector<int>> d;
+    for (int x = 0; x < rows; x++) {
+        vector<int> row;
+        for (int y = 0; y < columns; y++) {
+            int cellValue = distribution(generator);
+            row.push_back(cellValue);
+        }
+        d.push_back(row);
+    }
+
+    return d;
+}
+
+int calculateMinimumKnightHitPointsToRescuePrincess(const vector<vector<int>>& dungeon) {
+
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(0,1);
+
+    int width = dungeon[0].size() - 1;
+    int height = dungeon.size() - 1;
+
+    // dungeon[y][x]. All moves must increment x OR Y.
+    // Start location dungeon[0][0].
+    // goal location is dungeon[height][width]
+    int bestScore = numeric_limits<int>::min();
+    int iterations = pow(width,height) * 10;
+    while (iterations-- > 0) {
+//        cout << "\nnew iteration" << endl;
+        int currentPosX = 0;
+        int currentPosY = 0;
+        int currentScore = 0;
+        currentScore += dungeon[currentPosY][currentPosX];
+        while (true) {
+//            cout << "pos: (" << currentPosY << ", " << currentPosX<< "), cell value: " << dungeon[currentPosY][currentPosX] << endl;
+
+            bool moved = false;
+            int randomMove = distribution(generator);
+            if (randomMove == 1) {
+                // try move right
+                if (currentPosX != width) {
+                    currentPosX++;
+//                    cout << "RIGHT->";
+                    moved = true;
+                }
+            } else {
+                // try move down
+                if (currentPosY != height) {
+                    currentPosY++;
+//                    cout << "DOWN->";
+                    moved = true;
+                }
+            }
+            if (moved) {
+                currentScore += dungeon[currentPosY][currentPosX];
+//                cout << " currentscore: " << currentScore << endl;
+            }
+
+            // if currentScore is negative then knight is dead; - break
+            if (currentScore < 0) {
+                // cout << "Knight is dead, so break";
+                //break;
+            }
+            // Is the knight in the princess square bottom right?
+            if ( (currentPosX == width) && (currentPosY == height)) {
+                if (currentScore > bestScore) {
+                    bestScore = currentScore;
+//                    cout << "New best score: " << bestScore << endl;
+                }
+                break;
+            }
+        }
+    }
+    if (bestScore < 0)
+        bestScore = abs(bestScore);
+    else
+        bestScore = -bestScore;
+//    cout << endl << "returning best score: " << bestScore << endl;
+    return bestScore;
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: a.out dungeon_rows dungeon_columns" << endl;
+        return -1;
+    }
+    int rows = 3 ;
+    int columns = 3;
+    try {
+        rows = stoi(argv[1]);
+        columns = stoi(argv[2]);
+    }
+    catch(exception& e) {
+        cout << "could not understand input using 3x3 dungeon grid!" << endl;
+        rows = 3 ;
+        columns = 3;
+    }
+
+    auto dungeon = randomMatrix(rows, columns, -4, 2);
+    printDungeon(dungeon);
+    int hp = calculateMinimumKnightHitPointsToRescuePrincess(dungeon);
+    cout << "Minimum Knight HitPoints to rescue princess: " << hp << endl;
+}
